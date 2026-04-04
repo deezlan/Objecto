904111ffa352097dff56b6622e8060f2258977db
Danial Eizlan Daud <64958824+deezlan@users.noreply.github.com>
2026-04-04 07:12:00 +0100

    feat(avatar): add networked floating name tags with local player hiding (#4)
    * feat(avatar): add networked floating name tags above player avatars
    
    * fix(avatar): detach name tag from head bone and fix camera facing rotation

---
+++ UnityClient/Assets/Scripts/NetworkedName.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Fusion;
+
+public class NetworkedName : NetworkBehaviour
+{
+    [SerializeField] private TextMeshProUGUI nameTagText;
+    [SerializeField] private GameObject nameTagObject;
+
+    [Networked, OnChangedRender(nameof(OnNameChanged))]
+    public NetworkString<_16> PlayerName { get; set; }
+
+    public override void Spawned()
+    {
+        if (Object.HasStateAuthority)
+        {
+            // Hide own name tag
+            nameTagObject.SetActive(false);
+            // Set name directly since we have state authority
+            PlayerName = "Player " + Object.StateAuthority.PlayerId;
+        }
+        
+        // Always update text on spawn for late joiners
+        nameTagText.text = PlayerName.ToString();
+    }
+
+    private void OnNameChanged()
+    {
+        nameTagText.text = PlayerName.ToString();
+    }
+
+    private bool IsLocalNetworkRig()
+    {
+        return Object.HasStateAuthority;
+    }
+}
